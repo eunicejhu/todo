3b190d2769806dbff59ed589178a5e4b9bb5c77b
ZUOQIN HU (Isabella) <eunicejhu@gmail.com>
2022-03-05 11:38:28 +0100

    config: setup nextjs, ts config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
+const path = require("path");
+const { merge } = require("webpack-merge");
+
 module.exports = {
   reactStrictMode: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Important: return the modified config
+    const overwrittenConf = {
+      resolve: {
+        alias: {
+          src: path.resolve(__dirname, "src"),
+        },
+      },
+    };
+    return merge(config, overwrittenConf);
+  },
 };
