5a12ccebd9d2d1b2506b6b0df703f311d029c68e
ZUOQIN HU (Isabella) <eunicejhu@gmail.com>
2022-03-07 10:28:17 +0100

    feat: adjust theme

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -81,7 +81,7 @@ export default function SignIn() {
               alignItems: "center",
             }}
           >
-            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
               <FormatListNumberedIcon />
             </Avatar>
             <Typography component="h1" variant="h5">
